6175582f5e295dff58d9f0c1b062f8cb58923664
Mihail Luchkin <mihail.luchkin@lamoda.ru>
2022-10-07 14:14:00 +0300

    hw09_struct_validator

--- hw09_struct_validator/validator.go
+++ hw09_struct_validator/validator.go
@@ -1,17 +1,421 @@
 package hw09structvalidator
 
-type ValidationError struct {
-	Field string
-	Err   error
+import (
+	"bytes"
+	"errors"
+	"fmt"
+	"reflect"
+	"regexp"
+	"strconv"
+	"strings"
+)
+
+type (
+	// Все валидаторы объедены общим интерфейсом.
+	FieldValidator interface {
+		ValidateField(field reflect.StructField, value reflect.Value) ValidationError
+	}
+
+	// Валидатор проверяет вхождение значения в множество.
+	InStringFieldValidator struct {
+		in map[string]struct{}
+	}
+
+	// Валидатор проверяет длину строки.
+	LengthStringFieldValidator struct {
+		length int
+	}
+
+	// Валидатор проверяет соответствие регулярному выражению.
+	RegexpStringFieldValidator struct {
+		regexp *regexp.Regexp
+	}
+
+	// Валидатор проверяет вхождение значения в множество.
+	InIntFieldValidator struct {
+		in map[int]struct{}
+	}
+
+	// Валидатор проверяет что значение больше заданного в описании тега.
+	MinIntFieldValidator struct {
+		min int
+	}
+
+	// Валидатор проверяет что значение меньше заданного в описании тега.
+	MaxIntFieldValidator struct {
+		max int
+	}
+
+	// Ошибка валидации возникающая если проверка не прошла.
+	ValidationError struct {
+		Field string
+		Err   error
+	}
+
+	// Ошибка валидации возникающая если при создании какого-то поля произошла ошибка.
+	CreationError struct {
+		Field string
+		Err   error
+	}
+
+	// Ошибки валидации объединенные в массив.
+	ValidationErrors []ValidationError
+)
+
+var (
+	ErrIsNotStruct           = errors.New("is not struct")
+	ErrHasUnknownValidator   = errors.New("has unknown validator")
+	ErrHasInvalidValidator   = errors.New("has invalid validator")
+	ErrWhanCreatingValidator = errors.New("when creating validator")
+
+	ErrValidateLessValue       = errors.New("value is less than expected")
+	ErrValidateOutOfScopeValue = errors.New("value out of scope")
+	ErrValidateGreaterValue    = errors.New("value is greater than expected")
+	ErrValidateStringLength    = errors.New("string length does not match expected")
+	ErrValidateRegularMatch    = errors.New("value does not match regular expression")
+
+	NilValidationError        ValidationError
+	mappingCreateFuncWithKind = map[reflect.Kind]map[string]func(cond string) (FieldValidator, error){
+		reflect.String: {
+			"in":     NewInStringValidator,
+			"len":    NewLengthStringValidator,
+			"regexp": NewRegexpStringFieldValidator,
+		},
+		reflect.Int: {
+			"in":  NewInIntValidator,
+			"min": NewMinIntValidator,
+			"max": NewMaxIntValidator,
+		},
+	}
+)
+
+func NewMinIntValidator(cond string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	number, err := strconv.Atoi(cond)
+	if err != nil {
+		return validator, err
+	}
+
+	validator = &MinIntFieldValidator{
+		min: number,
+	}
+
+	return validator, nil
 }
 
-type ValidationErrors []ValidationError
+func (fv *MinIntFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]int64) {
+			if v < int64(fv.min) {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateLessValue,
+				}
+			}
+		}
+	}
+
+	if value.Int() < int64(fv.min) {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateLessValue,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewInIntValidator(cond string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	inValues := make(map[int]struct{})
+	for _, value := range strings.Split(cond, ",") {
+		val, err := strconv.Atoi(value)
+		if err != nil {
+			return validator, err
+		}
+		inValues[val] = struct{}{}
+	}
+
+	validator = &InIntFieldValidator{
+		in: inValues,
+	}
+
+	return validator, nil
+}
+
+func (fv *InIntFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]int) {
+			if _, ok := fv.in[v]; !ok {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateOutOfScopeValue,
+				}
+			}
+		}
+	}
+
+	if _, ok := fv.in[int(value.Int())]; !ok {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateOutOfScopeValue,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewMaxIntValidator(cond string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	m, err := strconv.Atoi(cond)
+	if err != nil {
+		return validator, err
+	}
+
+	validator = &MaxIntFieldValidator{
+		max: m,
+	}
+
+	return validator, nil
+}
+
+func (fv *MaxIntFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]int64) {
+			if v > int64(fv.max) {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateGreaterValue,
+				}
+			}
+		}
+	}
+
+	if value.Int() > int64(fv.max) {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateGreaterValue,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewLengthStringValidator(cond string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	length, err := strconv.Atoi(cond)
+	if err != nil {
+		return validator, err
+	}
+
+	validator = &LengthStringFieldValidator{
+		length: length,
+	}
+
+	return validator, nil
+}
+
+func (fv *LengthStringFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]string) {
+			if len(v) != fv.length {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateStringLength,
+				}
+			}
+		}
+	}
+
+	if len(value.String()) != fv.length {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateStringLength,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewInStringValidator(values string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	inValues := make(map[string]struct{})
+	for _, val := range strings.Split(values, ",") {
+		inValues[val] = struct{}{}
+	}
+
+	validator = &InStringFieldValidator{
+		in: inValues,
+	}
+
+	return validator, nil
+}
+
+func (fv *InStringFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]string) {
+			if _, ok := fv.in[v]; !ok {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateOutOfScopeValue,
+				}
+			}
+		}
+	}
+
+	if _, ok := fv.in[value.String()]; !ok {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateOutOfScopeValue,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewRegexpStringFieldValidator(exp string) (FieldValidator, error) {
+	var validator FieldValidator
+
+	reg, err := regexp.Compile(exp)
+	if err != nil {
+		return validator, err
+	}
+
+	validator = &RegexpStringFieldValidator{
+		regexp: reg,
+	}
+
+	return validator, nil
+}
+
+func (fv *RegexpStringFieldValidator) ValidateField(field reflect.StructField, value reflect.Value) ValidationError {
+	kind := field.Type.Kind()
+	if kind == reflect.Slice || kind == reflect.Array {
+		for _, v := range value.Interface().([]string) {
+			if !fv.regexp.MatchString(v) {
+				return ValidationError{
+					Field: field.Name,
+					Err:   ErrValidateRegularMatch,
+				}
+			}
+		}
+	}
+
+	if !fv.regexp.MatchString(value.String()) {
+		return ValidationError{
+			Field: field.Name,
+			Err:   ErrValidateRegularMatch,
+		}
+	}
+
+	return NilValidationError
+}
+
+func NewFieldValidators(f reflect.StructField, k reflect.Kind) ([]FieldValidator, error) {
+	var validators []FieldValidator
+
+	tags, ok := f.Tag.Lookup("validate")
+	if !ok {
+		return validators, nil
+	}
+
+	createFuncMapping, ok := mappingCreateFuncWithKind[k]
+	if !ok {
+		return validators, ErrHasUnknownValidator
+	}
+	for _, validationRules := range strings.Split(tags, "|") {
+		if ruleCondition := strings.Split(validationRules, ":"); len(ruleCondition) == 2 {
+			createFunc, ok := createFuncMapping[ruleCondition[0]]
+			if !ok {
+				return validators, ErrHasUnknownValidator
+			}
+			validator, err := createFunc(ruleCondition[1])
+			if err != nil {
+				return validators, CreationError{
+					Field: f.Name,
+					Err:   err,
+				}
+			}
+			validators = append(validators, validator)
+		} else {
+			return validators, ErrHasInvalidValidator
+		}
+	}
+
+	return validators, nil
+}
+
+func (e CreationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Field, e.Err)
+}
+
+func (e ValidationError) Error() string {
+	return fmt.Sprintf("%s: %s", e.Field, e.Err)
+}
 
 func (v ValidationErrors) Error() string {
-	panic("implement me")
+	var buffer bytes.Buffer
+
+	if len(v) > 0 {
+		buffer.WriteString("Errors:\n")
+	}
+	for _, err := range v {
+		buffer.WriteString(fmt.Sprintf("- %s\n", err.Error()))
+	}
+
+	return buffer.String()
 }
 
 func Validate(v interface{}) error {
-	// Place your code here.
+	// Переданное тип переданного значения не является валидной структурой или указателем на нее
+	r := reflect.Indirect(reflect.ValueOf(v))
+	if r.Type().Kind() != reflect.Struct {
+		return ErrIsNotStruct
+	}
+
+	validators := make(map[string][]FieldValidator, 0)
+	for i := 0; i < r.NumField(); i++ {
+		structField := r.Type().Field(i)
+		fieldValue := r.Field(i)
+		kind := structField.Type.Kind()
+		if kind == reflect.Slice || kind == reflect.Array {
+			if fieldValue.Len() == 0 {
+				continue
+			}
+			kind = fieldValue.Index(0).Type().Kind()
+		}
+		fieldValidators, err := NewFieldValidators(structField, kind)
+		if err == nil && len(fieldValidators) > 0 {
+			validators[structField.Name] = fieldValidators
+		} else if err != nil {
+			return err
+		}
+	}
+
+	validationErrors := make(ValidationErrors, 0)
+	for name, fieldValidators := range validators {
+		field, _ := r.Type().FieldByName(name)
+		value := r.FieldByName(name)
+		for _, v := range fieldValidators {
+			if err := v.ValidateField(field, value); err != NilValidationError {
+				validationErrors = append(validationErrors, err)
+			}
+		}
+	}
+
+	if len(validationErrors) > 0 {
+		return validationErrors
+	}
+
 	return nil
 }
--- hw09_struct_validator/validator_test.go
+++ hw09_struct_validator/validator_test.go
@@ -3,7 +3,10 @@ package hw09structvalidator
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 type UserRole string
@@ -34,18 +37,167 @@ type (
 		Code int    `validate:"in:200,404,500"`
 		Body string `json:"omitempty"`
 	}
+
+	invalidTag struct {
+		age int `validate:"in:admin,stuff"`
+	}
+
+	invalidType struct {
+		char byte `validate:"in:admin,stuff"`
+	}
 )
 
+func TestIntFieldValidator(t *testing.T) {
+	var validators []FieldValidator
+
+	// Валидация полей структуры User
+	user := User{
+		Age: 21,
+	}
+
+	// Если значение является указателем, то вернуть значение за указателем
+	userValue := reflect.Indirect(reflect.ValueOf(user))
+
+	// Узнать тип атрибута
+	ageFieldValue := userValue.FieldByName("Age")
+	ageStructField, _ := userValue.Type().FieldByName("Age")
+	kind := ageStructField.Type.Kind()
+	validators, _ = NewFieldValidators(ageStructField, kind)
+
+	require.True(t, ageFieldValue.Type().Kind() == reflect.Int)
+	require.Len(t, validators, 2)
+	for _, validator := range validators {
+		require.True(t, validator.ValidateField(ageStructField, ageFieldValue) == NilValidationError)
+	}
+
+	// Валидация полей структуры Response
+	response := Response{
+		Code: 301,
+	}
+	responseValue := reflect.Indirect(reflect.ValueOf(response))
+	// Узнать тип атрибута
+	codeFieldValue := responseValue.FieldByName("Code")
+	codeStructField, _ := responseValue.Type().FieldByName("Code")
+	kind = codeStructField.Type.Kind()
+	validators, _ = NewFieldValidators(codeStructField, kind)
+	require.Len(t, validators, 1)
+	for _, validator := range validators {
+		require.Error(t, validator.ValidateField(codeStructField, codeFieldValue))
+	}
+}
+
+func TestStringFieldValidator(t *testing.T) {
+	var validators []FieldValidator
+	user := User{
+		ID:     "007",
+		Phones: []string{"71111111111", "7111111111"},
+		Email:  "007agent",
+		Role:   "troll",
+	}
+
+	// Если значение является указателем, то вернуть значение за указателем
+	userValue := reflect.Indirect(reflect.ValueOf(user))
+
+	// Узнать тип атрибута
+	idFieldValue := userValue.FieldByName("ID")
+	idStructField, _ := userValue.Type().FieldByName("ID")
+	require.True(t, idFieldValue.Type().Kind() == reflect.String)
+	kind := idStructField.Type.Kind()
+	validators, _ = NewFieldValidators(idStructField, kind)
+	for _, validator := range validators {
+		require.Error(t, validator.ValidateField(idStructField, idFieldValue))
+	}
+
+	// Узнать то, что атрибут является слайсом и тип его элементов
+	phonesFieldValue := userValue.FieldByName("Phones")
+	phonesStructField, _ := userValue.Type().FieldByName("Phones")
+	require.True(t, phonesFieldValue.Type().Kind() == reflect.Slice)
+	if phonesFieldValue.Type().Kind() == reflect.Slice && phonesFieldValue.Len() > 0 {
+		require.True(t, phonesFieldValue.Index(0).Type().Kind() == reflect.String)
+	}
+	validators, _ = NewFieldValidators(phonesStructField, reflect.String)
+	for _, validator := range validators {
+		require.Error(t, validator.ValidateField(phonesStructField, phonesFieldValue))
+	}
+
+	// Проверить на соответсвие регулярному выражению
+	emailFieldValue := userValue.FieldByName("Email")
+	emailStructField, _ := userValue.Type().FieldByName("Email")
+	kind = emailStructField.Type.Kind()
+	validators, _ = NewFieldValidators(emailStructField, kind)
+	for _, validator := range validators {
+		require.Error(t, validator.ValidateField(emailStructField, emailFieldValue))
+	}
+
+	// // Обойти все элементы слайса (через его приведение к исходному типу)
+	// phones := phonesField.Interface().([]string)
+	// for _, v := range phones {
+	// 	fmt.Println(v)
+	// }
+
+	// Убедиться в том, что можно работать с значением, основанное на простом типе
+	roleFieldValue := userValue.FieldByName("Role")
+	roleStructField, _ := userValue.Type().FieldByName("Role")
+	require.True(t, roleFieldValue.Type().Kind() == reflect.String)
+	kind = roleStructField.Type.Kind()
+	validators, _ = NewFieldValidators(roleStructField, kind)
+	for _, validator := range validators {
+		require.Error(t, validator.ValidateField(roleStructField, roleFieldValue))
+	}
+}
+
 func TestValidate(t *testing.T) {
+	explicitErrorTests := []struct {
+		in          interface{}
+		expectedErr error
+	}{
+		{
+			in:          "qwerty",
+			expectedErr: ErrIsNotStruct,
+		},
+		{
+			in:          123,
+			expectedErr: ErrIsNotStruct,
+		},
+		{
+			in: invalidType{
+				char: '!',
+			},
+			expectedErr: ErrHasUnknownValidator,
+		},
+	}
+
+	for i, tt := range explicitErrorTests {
+		t.Run(fmt.Sprintf("case %d", i), func(t *testing.T) {
+			tt := tt
+			t.Parallel()
+
+			require.ErrorIs(t, tt.expectedErr, Validate(tt.in))
+		})
+	}
+
+	var validationError ValidationErrors
+	var creationError CreationError
 	tests := []struct {
 		in          interface{}
 		expectedErr error
 	}{
 		{
-			// Place your code here.
+			in: User{
+				ID:     "007",
+				Phones: []string{"71111111111", "7111111111"},
+				Email:  "007agent",
+				Role:   "troll",
+				meta:   json.RawMessage{'H', 'i', '!'},
+			},
+			expectedErr: validationError,
 		},
-		// ...
-		// Place your code here.
+		{
+			in: invalidTag{
+				age: 5,
+			},
+			expectedErr: creationError,
+		},
 	}
 
 	for i, tt := range tests {
@@ -53,8 +205,7 @@ func TestValidate(t *testing.T) {
 			tt := tt
 			t.Parallel()
 
-			// Place your code here.
-			_ = tt
+			require.ErrorAs(t, Validate(tt.in), &tt.expectedErr)
 		})
 	}
 }
